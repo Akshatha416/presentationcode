271d0e9c92b9e43c3968011a0abfcb4fc0b75dc5
Akshatha416 <107977782+Akshatha416@users.noreply.github.com>
2022-07-15 11:30:28 +0530

    Add files via upload
    first unwanted pgrms

---
+++ UserApp/src/main/java/com/example/demo/UserController/Controller.java
@@ -0,0 +1,38 @@
+package com.example.demo.UserController;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.example.demo.FlightAppEntities.FlightEntity;
+
+@RestController
+@CrossOrigin("*")
+public class Controller {
+	
+	//@Autowired FlightService flightService
+	
+	
+	//@GetMapping("/flights")
+	//public List<FlightEntity> getAllFlights(){
+		
+		//return flightService.getAllFlight();
+		
+	//}
+	
+	//@GetMapping("/flight")
+	//public FLightEntiy getFlightById(@Requestparam("id") Integer id) {
+		
+    //FlightEntity flightEntity = flightService.getFlightById(id);
+		
+		//return flightEntity;
+	//}
+	//@PostMapping("/adduser")
+	
+		
+	}
+	
+
+
